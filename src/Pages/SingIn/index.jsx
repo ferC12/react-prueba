395eb5774f6944456372c09b553b438e3a0179b8
Fernando Cigoy <fernandoc32667@gmail.com>
2023-06-29 20:46:10 -0300

    Vista dinámica para Sign In

--- src/Pages/SingIn/index.jsx
+++ src/Pages/SingIn/index.jsx
@@ -1,24 +1,37 @@
+import { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { ShoppingCartContext } from '../../Context'
 import Layout from '../../Components/Layout'
 
 function SingIn() {
+  const context = useContext(ShoppingCartContext)
+  const  [view, setView] = useState('user-info')
 
-  return (
-    <Layout>
-      <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+  //Account
+  const account = localStorage.getItem('account')
+  const parsedAccount = JSON.parse(account)
+
+  //With Account
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
+  const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
+
+  const renderLogIn = () => {
+    return (
       <div className='flex flex-col w-80'>
         <p>
           <span className='font-light text-sm'>Email: </span>
-          <span>juan@platzi.com</span>
+          <span>{parsedAccount?.email}</span>
         </p>
         <p>
           <span className='font-light text-sm'>Password: </span>
-          <span>******</span>
+          <span>{parsedAccount?.password}</span>
         </p>
         <Link
           to="/">
           <button
-            className='bg-black disabled:bg-black/40 text-white  w-full rounded-lg py-3 mt-4 mb-2'>
+            className='bg-black disabled:bg-black/40 text-white  w-full rounded-lg py-3 mt-4 mb-2'
+            disabled={!hasUserAnAccount}>
             Log in
           </button>
         </Link>
@@ -26,11 +39,25 @@ function SingIn() {
           <a className='font-light text-xs underline underline-offset-4' href='/'>Forgot my password</a>
         </div>
         <button
-          className='border border-black disabled:text-black/40 disabled:border-black/40
-          rounded-lg mt-6 py-3'>
+          className='border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3'
+          onClick={() => setView('create-user-info')}
+          disabled={hasUserAnAccount}>
           Sign up
         </button>
       </div>
+    )
+  }
+
+  const renderCreateUserInfo = () => {
+    // TODO: Create render view
+  }
+
+  const renderView = () => view === 'create-user-info' ? renderCreateUserInfo() : renderLogIn()
+
+  return (
+    <Layout>
+      <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+      {renderView()}
     </Layout>
   )
 }
